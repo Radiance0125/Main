182e2a6c7738047416f24975e54eb8685f4a5ade
Radiance0125 <s231019@uccke.edu.hk>
2024-07-10 18:24:27 +0800

    added info

--- testing.py
+++ testing.py
@@ -1,2 +1,13 @@
-print ("hello world")
-print ([i for i in "hello"])
+#this is for the 24-25 open day
+#base story (credits to Leah):
+#There was a reunion party in the mountains (It’s near a town so the area isn’t VERY secluded) and they had a picnic. They were watching the sunset when one of them said the trees blocked the view and they couldn’t take pictures, so they went through the trees to a nearby cliff to take pictures. Then, the others heard a yelp not long after and they rushed over to see what happened. They saw the victim’s camera on the ground and the victim was plummeting down. The victim was calling out, “(someone) pushed me!” But they couldn’t hear the name because he was already very far down.
+
+'''
+characters:
+ophelia : murderer
+laurence : victim
+veronica : friend (no role)
+katrina : friend (no role)
+nathan : friend (no role)
+'''
+
