b9a8f96943b5a905db6c4a06300f4a5ca18b6a99
Radiance0125 <s231019@uccke.edu.hk>
2024-07-25 17:03:36 +0800

    finished image and text

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 #this is for the 24-25 open day
 #base story (credits to Leah):
-#There was a reunion party in the mountains (It’s near a town so the area isn’t VERY secluded) and they had a picnic. They were watching the sunset when one of them said the trees blocked the view and they couldn’t take pictures, so they went through the trees to a nearby cliff to take pictures. Then, the others heard a yelp not long after and they rushed over to see what happened. They saw the victim’s camera on the ground and the victim was plummeting down. The victim was calling out, “(someone) pushed me!” But they couldn’t hear the name because he was already very far down.
+'''There was a reunion party in the mountains
+(It’s near a town so the area isn’t VERY secluded) and 
+they had a picnic. They were watching the sunset when 
+one of them said the trees blocked the view and they 
+couldn’t take pictures, so they went through the trees 
+to a nearby cliff to take pictures. Then, the others 
+heard a yelp not long after and they rushed over to see 
+what happened. They saw the victim’s camera on the ground 
+and the victim was plummeting down. The victim was calling 
+out, “(someone) pushed me!” But they couldn’t hear the name 
+because he was already very far down.'''
 
 '''
 characters:
@@ -16,4 +26,4 @@ detective
 '''
 
 def textbox(message):
-    
+    pass
--- testing.py
+++ testing.py
@@ -1,11 +1,21 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 root = tk.Tk()
 root.title("testing")
-root.geometry("400x200")
+root.geometry("400x400")
 
 
 #1st approach
-photo = tk.PhotoImage(file = "image.jpg")
+photo = tk.PhotoImage(file = "assets/image.png")
+
+#2nd approach
+photo2 = Image.open("assets/image.png")
+resized_image = photo2.resize((400, 400), Image.BILINEAR)
+converted_image = ImageTk.PhotoImage(resized_image)
+
+
+label = tk.Label(root, image = converted_image, bg = "white", fg = "black")
+
 
 label.pack()
 root.mainloop()
