3edc2939d31bb2a687554820fee9fe2258fb3cfb
Radiance0125 <s231019@uccke.edu.hk>
2024-07-26 16:58:12 +0800

    buttons and grid

--- testing.py
+++ testing.py
@@ -10,9 +10,13 @@ label = tk.Label(root, text = "hello world", font = ("arial", 18))
 label.pack(padx = 20, pady = 20)
 
 textbox = tk.Text(root, height = 3, font = ("arial", 16))
-textbox.pack(padx = 10)
+textbox.pack(padx = 10, pady = 10)
 
 button = tk.Button(root, text = "click me!", font = ("arial", 18))
-button.pack()
+button.pack(padx = 10, pady = 10)
+
+anotherbtn = tk.Button(root, text = "test")
+anotherbtn.place (x = 200, y = 300, height = 100, width = 100)
+
 
 root.mainloop()
