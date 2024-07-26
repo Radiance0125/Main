f9d0dd3898152df67424c38b9ba01fa488d96e27
Radiance0125 <s231019@uccke.edu.hk>
2024-07-26 16:22:35 +0800

    gui testing

---
+++ image_display.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+root = tk.Tk()
+root.title("testing")
+root.geometry("400x400")
+
+photo2 = Image.open("assets/image.png")
+resized_image = photo2.resize((400, 400), Image.BILINEAR)
+converted_image = ImageTk.PhotoImage(resized_image)
+
+label = tk.Label(root, image = converted_image, bg = "white", fg = "black")
+
+label.pack()
+root.mainloop()
