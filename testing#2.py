7e7e8de661048dfb99a371d1ba5e152726b12626
Radiance0125 <s231019@uccke.edu.hk>
2024-07-27 00:09:31 +0800

    keypad done

--- testing#2.py
+++ testing#2.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 root = tk.Tk()
 
-root.geometry("500x500")
+root.geometry("600x600")
 
 root.title("my first gui")
 
@@ -16,6 +16,7 @@ buttonframe = tk.Frame(root)
 buttonframe.columnconfigure(0, weight = 1)
 buttonframe.columnconfigure(1, weight = 1)
 buttonframe.columnconfigure(2, weight = 1)
+buttonframe.columnconfigure(3, weight = 1)
 
 btn1 = tk.Button(buttonframe, text = "1", font = ("arial", 18))
 btn1.grid(row = 0, column = 0, sticky = tk.W + tk.E)
@@ -34,6 +35,18 @@ btn5.grid(row = 1, column = 1, sticky = tk.W + tk.E)
 
 btn6 = tk.Button(buttonframe, text = "6", font = ("arial", 18))
 btn6.grid(row = 1, column = 2, sticky = tk.W + tk.E)
+
+btn7 = tk.Button(buttonframe, text = "7", font = ("arial", 18))
+btn7.grid(row = 2, column = 0, sticky = tk.W + tk.E)
+
+btn8 = tk.Button(buttonframe, text = "8", font = ("arial", 18))
+btn8.grid(row = 2, column = 1, sticky = tk.W + tk.E)
+
+btn9 = tk.Button(buttonframe, text = "9", font = ("arial", 18))
+btn9.grid(row = 2, column = 2, sticky = tk.W + tk.E)
+
+btn0 = tk.Button(buttonframe, text = "0", font = ("arial", 18))
+btn0.grid(row = 3, column = 1, sticky = tk.W + tk.E)
 
 buttonframe.pack(fill = "x")
 
